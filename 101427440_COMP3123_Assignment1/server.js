9d9f4ab4f8827e1ddc010bf5f6ee08c104da4b8b
Haley <hs.haleyk@gmail.com>
2024-10-06 14:21:35 -0400

    db test code added 241006

--- 101427440_COMP3123_Assignment1/server.js
+++ 101427440_COMP3123_Assignment1/server.js
@@ -23,6 +23,27 @@ const app = express();
 
 app.use(express.json());
 
+
+
+
+
+
+// Test endpoint to check database connection
+app.get('/api/v1/test-db', async (req, res) => {
+  try {
+    await mongoose.connection.db.admin().ping();
+    res.status(200).send('Database connection is successful');
+  } catch (error) {
+    console.error('Database connection test failed:', error);
+    res.status(500).send('Database connection failed');
+  }
+});
+
+
+
+
+
+
 const userRouter = require('./routes/userRoutes');
 const empRouter = require('./routes/empRoutes');
 
