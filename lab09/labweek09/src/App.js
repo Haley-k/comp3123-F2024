bb42161b9905b89a5986a0701f29969362274c39
Haley <hs.haleyk@gmail.com>
2024-10-30 21:52:35 -0400

    exercise9 241030

---
+++ lab09/labweek09/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <h2>Welcome to Fullstack Development - I</h2>
+        <h3>React JS Programming Week09 Lab exercise</h3>
+        <h4>101427440</h4>
+        <h5>Haley Kim</h5>
+        <h6>George Brown College, Toronto</h6>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
